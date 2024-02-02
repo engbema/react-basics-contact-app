01ef970d7b473003f050a74bc9225dc15459cdc6
Abdifatah Yusuf <studentabdifatah@gmail.com>
2024-02-02 19:42:13 +0300

    adding react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import Header from "./Header";
@@ -31,9 +32,27 @@ function App() {
 
   return (
     <div className="ui container">
-      <Header />
-      <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} getContactId={removeContactHandler} />
+      <Router>
+        {/* <Header /> */}
+        <Routes>
+          <Route
+            path="/add"
+            render={(props) => (
+              <AddContact {...props} addContactHandler={addContactHandler} />
+            )}
+          />
+          <Route
+            path="/"
+            render={(props) => (
+              <ContactList
+                {...props}
+                contacts={contacts}
+                getContactId={removeContactHandler}
+              />
+            )}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import ContactCard from "./ContactCard";
 
 const ContactList = (props) => {
@@ -17,7 +18,17 @@ const ContactList = (props) => {
     );
   });
 
-  return <div className="ui celled list">{renderContactList}</div>;
+  return (
+    <div className="main">
+      <h2>
+        Contact List{" "}
+        <Link to="/add">
+          <button className="ui button blue right">Add Contact</button>
+        </Link>
+      </h2>
+      <div className="ui celled list">{renderContactList}</div>
+    </div>
+  );
 };
 
 export default ContactList;
